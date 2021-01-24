94152c79bc3d834a3ee24edb633f18a859d65fd2
Marharyta Poliakovska <marharyta.poliakovska@ihsmarkit.com>
2021-01-24 14:39:14 +0200

    fix import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { ProductComponent } from './products/components/product/product.component';
 import { ProductListComponent } from './products/components/product-list/product-list.component';
-import { CartListComponent } from './card/components/cart-list/cart-list.component';
+import { CartListComponent } from './cart/components/cart-list/cart-list.component';
 
 @NgModule({
   declarations: [
