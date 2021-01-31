784f438c92a70f39491e3d82f9099f559e894ad7
Marharyta Poliakovska <marharyta.poliakovska@ihsmarkit.com>
2021-01-31 14:44:53 +0200

    chore: add changedetection to cart-item

--- src/app/cart/components/cart-item/cart-item.component.ts
+++ src/app/cart/components/cart-item/cart-item.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../models/cart-item.model';
 
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
-  styleUrls: ['./cart-item.component.scss']
+  styleUrls: ['./cart-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CartItemComponent {
 
